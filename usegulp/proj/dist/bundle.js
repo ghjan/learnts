f38b3e5d2a75413c9b186b7e3ec204df1efba4a5
jian.zhang <cajan2@163.com>
2018-08-23 14:45:56 +0800

    use babel(es2015)

--- usegulp/proj/dist/bundle.js
+++ usegulp/proj/dist/bundle.js
@@ -1,2 +1,25 @@
-!function i(u,f,c){function l(r,e){if(!f[r]){if(!u[r]){var t="function"==typeof require&&require;if(!e&&t)return t(r,!0);if(s)return s(r,!0);var n=new Error("Cannot find module '"+r+"'");throw n.code="MODULE_NOT_FOUND",n}var o=f[r]={exports:{}};u[r][0].call(o.exports,function(e){return l(u[r][1][e]||e)},o,o.exports,i,u,f,c)}return f[r].exports}for(var s="function"==typeof require&&require,e=0;e<c.length;e++)l(c[e]);return l}({1:[function(e,r,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0}),t.sayHello=function(e){return"Hello from "+e}},{}],2:[function(e,r,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n,o,i=e("./greet");n="greeting",o="TypeScript, Gulp, Browserify, tsify, watchify, gutil",document.getElementById(n).innerText=i.sayHello(o)},{"./greet":1}]},{},[2]);
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+function sayHello(name) {
+    return "Hello from " + name;
+}
+exports.sayHello = sayHello;
+
+},{}],2:[function(require,module,exports){
+"use strict";
+// import {sayHello} from "./greet";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+// console.log(sayHello("TypeScript"));
+var greet_1 = require("./greet");
+function showHello(divName, name) {
+    var elt = document.getElementById(divName);
+    elt.innerText = greet_1.sayHello(name);
+}
+showHello("greeting", "TypeScript, Gulp, Browserify, tsify, watchify, gutil");
+
+},{"./greet":1}]},{},[2])
+
 //# sourceMappingURL=bundle.js.map
